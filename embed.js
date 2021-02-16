0f35dccdbb760fe808ee913838d64d44a28f020b
HgeX <atomi2500@gmail.com>
2021-02-16 20:05:12 +0100

    embed stuff

---
+++ embed.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const discord = require('discord.js')
+
+module.exports = async (client, config, status) => {
+    const color = status.online ? '#1b913c' : '#d60018'
+    const embed = new Discord.MessageEmbed().setColor(color)
+        .setThumbnail(status.image)
+        .setTitle(config.name)
+        .setDescription(config.description)
+        .addField('Játékosok száma', `${status.playerCount} / ${status.maxPlayerCount}`)
+
+    const guild = await client.guilds.cache.fetch(config.guildID)
+    const channel = await guild.channels.cache.get(config.channelID)
+
+    await channel.send(embed)
+}
