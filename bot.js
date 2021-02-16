9bc49bc038a7085f6d0b5ab6bb1f0ad6261ba067
DaniFoldi <dani@stefan.hu>
2021-02-16 20:43:34 +0100

    Some improvements

--- bot.js
+++ bot.js
@@ -6,26 +6,54 @@ const fse = require('fs-extra')
 const json5 = require('json5')
 const minecraftUtil = require('minecraft-server-util')
 
-const CONFIG_FILE = process.env.CONFIG_FILE | 'config.json5'
+const embed = require('./embed')
+
+const CONFIG_FILE = process.env.CONFIG_FILE || 'config.json5'
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+const LAST_STATUS_FILE = process.env.CONFIG_FILE || 'laststatus.json5'
 
-;(async ()=>{
-  const configRaw = await fse.readFile('config.json5')
-  const config = json5.parse(configRaw.toString())
-  
-  // TODO check config gile
-  // TODO check discord token
-  
-  // TODO partials
-  const client = new discord.Client({partials: []})
-  
-  //client.login(DISCORD_TOKEN)
+const query = async function(config) {
+  try {
+    const newStatus = await minecraftUtil.status(config.ip, {port: config.port})
+    json5.stringify(newStatus)
+    
+    const status = {
+      online: true,
+      image: 'https://nuggetcraft.net/images/icons/server-icon.png',
+      playerCount: newStatus.onlinePlayers,
+      maxPlayerCount: newStatus.maxPlayers,
+      version: newStatus.version
+    }
+    
+    await fse.writeFile(LAST_STATUS_FILE, json5.stringify(status))
+    
+    return status
+  } catch (e) {
+    const statusRaw = await fse.readFile(LAST_STATUS_FILE)
+    const status = json5.parse(statusRaw.toString())
+    
+    return {online: false, playerCount: 0, ...status}
+  }
+}
 
-  status = {
-    online: true,
-    image: 'blabla.png',
-    'playerCount': 42,
-    'maxPlayerCount': 69,
-    'version': '1.69.420'
+const updateEmbed = async function(client, config) {
+  const status = await query(config)
+  const messageId = await embed(client, config, status)
+  // TODO debug update
+}
+
+;(async () => {
+  try {
+    const configRaw = await fse.readFile(CONFIG_FILE)
+    const config = json5.parse(configRaw.toString())
+
+    const client = new discord.Client({partials: ['USER', 'CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION']})
+
+    client.on('ready', async () => {updateEmbed(client, config)})
+    setInterval(async () => {updateEmbed(client, config)}, config.updateInterval)
+    
+    await client.login(DISCORD_TOKEN)
+  } catch (e) {
+    console.error(e)
   }
 })()
--- embed.js
+++ embed.js
@@ -1,18 +1,31 @@
 'use strict'
 
-const discord = require('discord.js')
+const {MessageEmbed} = require('discord.js')
 
 module.exports = async (client, config, status) => {
+  if (!client.readyTimestamp)
+    return
+    
+    
     const color = status.online ? '#1b913c' : '#d60018'
-    const embed = new Discord.MessageEmbed().setColor(color)
+    const embed = new MessageEmbed().setColor(color)
         .setThumbnail(status.image)
         .setTitle(config.name)
         .setDescription(config.description)
         .addField('Játékosok száma', `${status.playerCount} / ${status.maxPlayerCount}`)
         .setFooter(`${config.ip}${config.port === 25565 ? '' : (':' + config.port)}`)
 
-    const guild = await client.guilds.cache.fetch(config.guildID)
+    if (client.guilds.partial)
+      await client.guilds.fetch()
+    
+    const guild = await client.guilds.cache.get(config.guildID)
+    
+    if (guild.channels.partial)
+      await guild.channels.fetch()
+  
     const channel = await guild.channels.cache.get(config.channelID)
 
-    await channel.send(embed)
+    const message = await channel.send(embed)
+    
+    return message.id
 }
