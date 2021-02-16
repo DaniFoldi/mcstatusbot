1a43368ec5b56f05bb836d389edd0021fab6fec7
DaniFoldi <dani@stefan.hu>
2021-02-16 19:10:16 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const debug = require('debug')('mcstatusbot:bot')
+const discord = require('discord.js')
+const fse = require('fs-extra')
+const json5 = require('json5')
+const minecraftUtil = require('minecraft-server-util')
+
+const CONFIG_FILE = process.env.CONFIG_FILE | 'config.json5'
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+
+;(async ()=>{
+  const configRaw = await fse.readFile('config.json5')
+  const config = json5.parse(configRaw.toString())
+  
+  // TODO check config gile
+  // TODO check discord token
+  
+  // TODO partials
+  const client = new discord.Client({partials: []})
+  
+  client.login(DISCORD_TOKEN)
+})()
